c2e43ef3fba1dcf03cc54238096b10128eff98b5
Mojca Colnik <mojca.c13@gmail.com>
2021-04-03 21:56:52 +0200

    Reformat code

--- example.js
+++ example.js
@@ -6,14 +6,14 @@ console.log(generateSequence(13, 'waw'))
 
 console.log(generateSequence(5, '^*.*^'))
 
-console.log(generateSequence(12, (x, index) => index%2))
+console.log(generateSequence(12, (x, index) => index % 2))
 
-console.log(generateSequence(9, (x, index) => index%4))
+console.log(generateSequence(9, (x, index) => index % 4))
 
-console.log(generateSequence(10, (x, index) => index*3))
+console.log(generateSequence(10, (x, index) => index * 3))
 
-console.log(generateSequence(12, (x, index) => index+1))
+console.log(generateSequence(12, (x, index) => index + 1))
 
-console.log(generateSequence(6, (x, index) => index%3))
+console.log(generateSequence(6, (x, index) => index % 3))
 
 console.log(generateSequence(3))
